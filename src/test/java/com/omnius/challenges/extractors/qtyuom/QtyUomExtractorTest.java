83a2f696a3ef2feed0b507bdf7f05b591e901c45
ALI\Ali <m.ali1963@hotmail.com>
2018-05-21 17:23:39 +0200

    Added By Muhammad Ali

--- src/test/java/com/omnius/challenges/extractors/qtyuom/QtyUomExtractorTest.java
+++ src/test/java/com/omnius/challenges/extractors/qtyuom/QtyUomExtractorTest.java
@@ -1,31 +1,37 @@
-package com.omnius.challenges.extractors.qtyuom;
+package java.com.omnius.challenges.extractors.qtyuom;
 
-import org.junit.Assert;
-import org.junit.Test;
+import com.omnius.challenges.extractors.qtyuom.utils.LeftMostUOMExtractor;
 
 import com.omnius.challenges.extractors.qtyuom.QtyUomExtractor;
-import com.omnius.challenges.extractors.qtyuom.utils.LeftMostUOMExtractor;
 import com.omnius.challenges.extractors.qtyuom.utils.Pair;
+import org.junit.Assert;
+import org.junit.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 
 
 /**
- * 
+ *
  *
  * @author <a href="mailto:damiano@omnius.com">Damiano Giampaoli</a>
  * @since 25 Jan. 2018
  */
-public class QtyUomExtractorTest extends Assert{
-    
+public class QtyUomExtractorTest extends Assert {
+
     @Test
     public void extract_whenUOMIstheSecondTokenAndQTYTheFirst() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
-        Pair<String, String> result = matcher.extract("40 Stg. FR 60x40x3 Ã  6m 23 Stg. Ro 33,7x2 Ã  6m geschliffen 1.4301");
+        Pair<String, String> result = matcher.extract("40 stg. FR 60x40x3 Ã  6m 23 Stg. Ro 33,7x2 Ã  6m geschliffen 1.4301");
         String qty = result.getFirst();
         String uom = result.getSecond();
         assertEquals("40", qty);
         assertEquals("stg.", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_inLongDescription1() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -35,7 +41,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("900", qty);
         assertEquals("mm", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_inLongDescription2() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -45,7 +51,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000", qty);
         assertEquals("mm", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenUOMHasGermanCharacters() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -55,7 +61,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("100", qty);
         assertEquals("stück", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenThereAreSpacesAroundDecimalSeparator() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -65,7 +71,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("10,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparators() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -75,7 +81,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000000000", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparator() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -85,7 +91,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparatorWithNoSpaces() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -95,7 +101,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasNoDigitSeparatorsAndCommaAsDecimalSeparatorWithNoSpaces() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -105,7 +111,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparatorAndOtherDigits1() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -115,7 +121,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparatorAndOtherDigits2() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -125,7 +131,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("21000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparatorAndOtherDigits3() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -135,7 +141,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("331000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparatorAndOtherDigits4() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -145,7 +151,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityNumberHasSpaceDigitSeparatorsAndCommaAsDecimalSeparatorAndOtherDigits5() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -155,7 +161,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("001100000,00", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenQuantityIsSecondToken() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -165,7 +171,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("40", qty);
         assertEquals("stg.", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_issueFRA_332() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -175,7 +181,7 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("1000,00", qty);
         assertEquals("kg", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenDecimalSeparatorIsFirstOccurrence() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
@@ -185,31 +191,35 @@ public class QtyUomExtractorTest extends Assert{
         assertEquals("6", qty);
         assertEquals("stk", uom);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenDescriptionIsNull() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
         Pair<String, String> result = matcher.extract(null);
         assertNull(result);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenDescriptionIsEmpty() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
         Pair<String, String> result = matcher.extract("");
         assertNull(result);
     }
-    
+
     @Test
     public void extractQTYandUOM_whenDescriptionIsMadeOfBlankspaces() {
         QtyUomExtractor matcher = new LeftMostUOMExtractor();
         Pair<String, String> result = matcher.extract("\t\t\n   ");
         assertNull(result);
     }
-    
+
     @Test
-    public void computeAccuracy_checkIsHigherThan20Percent() {
-        // Load provided CSV
+    public void computeAccuracy_checkIsHigherThan20Percent() throws Exception{
+            // Load provided CSV
+        ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File(classLoader.getResource("resources/qty_uom_challenge_dataset_clean.csv").getFile());
+        System.out.println(file.getAbsolutePath());
+
         // For each line in the CSV run the LeftMostUOMExtractor
         // calculate how many corrected guess your algorithm compute
         // Assert the accuracy higher than 20%
